e42ac3c8f1a6ba8f1cf49cd8f60d35d39b734842
victor-delfino <hugo.delfino11@gmail.com>
2024-01-19 12:34:18 -0300

    Linked list - primeiro commit

---
+++ ExercícioLinkedList.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    // O head permanece no início da lista
+    printList(head);
+
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
